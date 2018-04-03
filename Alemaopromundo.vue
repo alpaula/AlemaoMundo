bb33dda7ae062ae65b1910c347b3bcfafd1bd368
Vai na Web <vainawebcpx@gmail.com>
2018-04-03 16:46:37 -0300

    download dos arquivos

---
+++ Alemaopromundo.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="AppSite">
+		<section class="container">
+			<img class="Image" src="../assets/LAC.jpg">
+			<div class="container-texts">
+				<h3 class="title">Casa Brota</h3>
+				<p class="caption">Coworking</p>
+			</div>
+		</section>
+	</div>
+</template>
+<script>
+	
+</script>
+<style scoped>
+	#AppSite{
+		background-color: yellow;
+		min-height: 100vh;
+	}
+	.container{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;	
+	}
+	.Image{
+		width: 90vh;
+		height: 60vh;
+
+	}
+	.container-texts{
+		color: white;
+			
+	}
+	.title{
+
+	}
+	.caption{
+
+	}
+	img{
+		width: 10px;
+	}
+</style>
